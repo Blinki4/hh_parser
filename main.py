17ae0d4146ca4b7afe483040e2b49b5e79baa5ec
Blinki4 <dmitriy.velg@mail.ru>
2026-03-15 22:53:44 +0300

    working prototype

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
-
+from selenium.webdriver.common.by import By
+import json
 
 def setup_driver() -> WebDriver:
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
@@ -15,10 +16,39 @@ def setup_driver() -> WebDriver:
 
 
 def main():
-    print('hello world!')
+    links = []
     driver = setup_driver()
     driver.get('https://hh.ru/search/vacancy?ored_clusters=true&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&L_save_area=true&text=Qa+automation+python')
-    time.sleep(4)
+    title_elements = driver.find_elements(By.XPATH, '//a[@data-qa="serp-item__title"]')
+    with open('links.txt', 'w') as file:
+        for el in title_elements:
+            link = el.get_attribute('href')
+            links.append(link)
+            file.write(f'{link}\n')
+
+    # driver.get('https://hh.ru/vacancy/131099127?query=Qa+automation+python&hhtmFrom=vacancy_search_list')
+    # skills_elements = driver.find_elements(By.XPATH, '//li[@data-qa="skills-element"]')
+    # skills = []
+    # for skill in skills_elements:
+    #     skills.append(skill.text)
+    #     print(skill.text)
+    # result[f'{link}'] = skills
+    # print(result)
+    result = {}
+    for link in links:
+        driver.get(link)
+        skills_elements = driver.find_elements(By.XPATH, '//li[@data-qa="skills-element"]')
+        skills = []
+        for skill in skills_elements:
+            skills.append(skill.text)
+            print(skill.text)
+        result[f'{link}'] = skills
+
+    print(result)
+
+    with open('result.txt', 'w') as file:
+        file.write(json.dumps(result, indent=4))
+
     driver.quit()
 
 
