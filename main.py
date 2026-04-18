67a5e7adde7c8f656b25fd00707ee6479cb74d20
Blinki4 <dmitriy.velg@mail.ru>
2026-04-18 23:28:51 +0300

    .

--- main.py
+++ main.py
@@ -6,20 +6,29 @@ from helpers.format_query import format_query
 from helpers.config import config_driver
 from helpers.lead_time import lead_time
 
+
+PROCESS_COUNT = 4
+
 @lead_time
 def main():
     query = format_query(sys.argv[1])
-    driver = config_driver()
-    parser = Parser(driver, query)
+    # driver = config_driver()
+    # parser = Parser(driver, query)
+    parser = Parser(query)
     writer = Writer()
-    parser.get_links()
+
+    parser.collect_links()
+    links = parser.get_links() # МБ тут надо закрывать драйвер
+    links_chunk = int(len(links) / PROCESS_COUNT)
+    print(links_chunk, 'CHUNK')
+
     writer.write_links(parser.links)
     jobs_data = parser.try_parse_jobs()
     skills = parser.get_job_skills(jobs_data)
     writer.write_job_data(jobs_data)
     writer.write_skills(skills)
     print('Результаты сформированы в директории results')
-    driver.quit()
+    parser.driver.quit()
 
 
 if __name__ == '__main__':
--- parser/parser.py
+++ parser/parser.py
@@ -1,17 +1,24 @@
+from helpers.config import config_driver
 from models.job_data import Job
 from pages.job_page import JobPage
 from pages.search_page import SearchPage
 
 
-class Parser:
-    links: list[str] = []
-    parsedJobsList: list[Job] = []
+# TODO Разделить парсер вакансий с парсером ссылок.
+#  После сбора ссылок закрывать драйвер в пуле открывать драйвер, парсить чанк ссылок и закрывать драйвер
+# Таким образом получится, что мы откроем 4 драйвера, каждый из которых парсит свой чанк, всю дату собирать в объект
+# Потом 4 таких объекта объединить в один и вывести в файл
 
-    def __init__(self, driver, query):
+
+class Parser:
+    def __init__(self, query):
         self.query = query
-        self.search_page = SearchPage(driver, self.query)
-        self.job_page = JobPage(driver)
+        self.driver = config_driver()
+        self.search_page = SearchPage(self.driver, self.query)
+        self.job_page = JobPage(self.driver)
         self.search_page.open(self.search_page.url)
+        self.links: list[str] = []
+        self.parsedJobsList: list[Job] = []
 
     def _get_pages_count(self) -> int:
         pages_count = self.search_page.get_pages_count()
@@ -19,10 +26,13 @@ class Parser:
         return pages_count
 
 
-    def get_links(self) -> None:
+    def collect_links(self) -> None:
         self.links = self.search_page.collect_links(self._get_pages_count())
         print(f'Всего найдено вакансий: {len(self.links)}')
 
+
+    def get_links(self):
+        return self.links
 
     def try_parse_jobs(self) -> list[Job]:
         try:
@@ -34,18 +44,22 @@ class Parser:
 
     def _parse_jobs(self) -> list[Job]:
         for i in range(len(self.links)):
-            self.job_page.open(self.links[i])
-            job_data = Job(
-                name=self.job_page.job_name,
-                link=self.links[i],
-                skills=self._get_skills_from_job(),
-                salary=self.job_page.salary,
-                experience=self.job_page.experience,
-                work_format=self.job_page.work_format
-            )
-            self.parsedJobsList.append(job_data.model_dump())
+            self._parse_one_job(self.links[i])
             print(i + 1)
         return self.parsedJobsList
+
+
+    def _parse_one_job(self, link):
+        self.job_page.open(link)
+        job_data = Job(
+            name=self.job_page.job_name,
+            link=link,
+            skills=self._get_skills_from_job(),
+            salary=self.job_page.salary,
+            experience=self.job_page.experience,
+            work_format=self.job_page.work_format
+        )
+        self.parsedJobsList.append(job_data.model_dump())
 
     def _get_skills_from_job(self) -> list[str]:
         skills: list[str] = []
