09f3f4a02f725a0e4278d2edd6998673f06be386
Blinki4 <dmitriy.velg@mail.ru>
2026-04-18 23:43:06 +0300

    Вынесена логика парсера ссылок в отдельный класс

--- helpers/config.py
+++ helpers/config.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 def config_driver() -> WebDriver:
     options = Options()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--window-size=1920,1080')
     options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=options)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+from parser.links_parser import LinksParser
 from parser.parser import Parser
 from writer.writer import Writer
 from helpers.format_query import format_query
@@ -14,15 +15,21 @@ def main():
     query = format_query(sys.argv[1])
     # driver = config_driver()
     # parser = Parser(driver, query)
-    parser = Parser(query)
+    links_parser = LinksParser(query)
     writer = Writer()
 
-    parser.collect_links()
-    links = parser.get_links() # МБ тут надо закрывать драйвер
-    links_chunk = int(len(links) / PROCESS_COUNT)
-    print(links_chunk, 'CHUNK')
+    # parser.collect_links()
+    # links = parser.get_links() # МБ тут надо закрывать драйвер
+    # links_chunk = int(len(links) / PROCESS_COUNT)
+    # print(links_chunk, 'CHUNK')
 
-    writer.write_links(parser.links)
+    links = links_parser.collect_links()
+
+    writer.write_links(links)
+
+
+    parser = Parser(query, links)
+
     jobs_data = parser.try_parse_jobs()
     skills = parser.get_job_skills(jobs_data)
     writer.write_job_data(jobs_data)
