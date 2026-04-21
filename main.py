dbe6073b499417e5887a998bfe41900bf4a4123e
Blinki4 <dmitriy.velg@mail.ru>
2026-04-21 18:03:02 +0300

    по крайней мере все работает

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from parser.links_parser import LinksParser
 from writer.writer import Writer
 from helpers.format_query import format_query
 from helpers.lead_time import lead_time
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from typing import Any
 
 
 
-PROCESS_COUNT = 5
+PROCESS_COUNT = 5 # Пиздеж
 
 def split_list_into_chunks(array: list[Any], full_chunks_count) -> list[list[Any]]:
     full_chunk, remainder = divmod(len(array), full_chunks_count)
@@ -28,9 +28,6 @@ def split_list_into_chunks(array: list[Any], full_chunks_count) -> list[list[Any
 def parse_chunk(links_chunk):
     jobs_parser = JobsParser()
     parsed_jobs = jobs_parser.parse_jobs(links_chunk)
-    print('PARSED_JOBS', parsed_jobs)
-    skills = jobs_parser.get_all_sorted_skills()
-    print('SKILLS', skills)
 
 
 @lead_time
@@ -44,22 +41,14 @@ def main():
 
     file_writer.write_links(links)
 
-    # full_chunk, remainder_chunk = divmod(len(links), PROCESS_COUNT)
-    # print('CHUNK_COUNT', full_chunk, remainder_chunk)
-    # all_chunks = []
-    # chunk = []
-    #
-    #
-    # for link in links:
-    #     chunk.append(link)
-    #     if len(chunk) == full_chunk:
-    #         all_chunks.append(chunk)
-    #         chunk = []
-    # all_chunks.append(chunk)
 
     chunks = split_list_into_chunks(links, PROCESS_COUNT)
 
     print(json.dumps(chunks, indent=4), 'CHUNKS')
+    print(len(chunks), 'chunks count')
+
+
+    result = []
 
     processes = []
     for i in chunks:
@@ -74,8 +63,10 @@ def main():
 
     # file_writer.write_job_data(parsed_jobs)
     # file_writer.write_skills(skills)
+
     print('Результаты сформированы в директории results')
 
+    print(result)
 
 if __name__ == '__main__':
     main()
