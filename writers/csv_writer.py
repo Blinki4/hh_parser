61739530534bfcf07b9c18ffba93ffdde5205070
Blinki4 <dmitriy.velg@mail.ru>
2026-04-25 18:04:52 +0300

    refactored

--- writers/csv_writer.py
+++ writers/csv_writer.py
@@ -3,9 +3,9 @@ from writers.writer import Writer
 from models.job import Job
 
 class CsvWriter(Writer):
-
-    def write_parsed_jobs(self, parsed_jobs):
-        mapping = {
+    def __init__(self):
+        super().__init__()
+        self.mapping = {
             'name' : 'Название',
             'salary' : 'Зарплата',
             'experience' : 'Опыт работы',
@@ -13,23 +13,31 @@ class CsvWriter(Writer):
             'skills' : 'Ключевые навыки',
             'link' : 'Ссылка'
         }
-        field_names = []
-        for key, value in mapping.items():
-            field_names.append(value)
 
+
+    def write_parsed_jobs(self, parsed_jobs):
+        translated = self.translate_keys(parsed_jobs)
+        with open(self.output_dir / 'result.csv', 'w', newline='', encoding='utf-8-sig') as file:
+            writer = csv.DictWriter(file, fieldnames=self._make_field_names())
+            writer.writeheader()
+            writer.writerows(translated)
+
+
+    def _make_field_names(self):
+        field_names = []
+        for key, value in self.mapping.items():
+            field_names.append(value)
+        return field_names
+
+
+    def translate_keys(self, parsed_jobs):
         translated = []
         for job in parsed_jobs:
             translated_job = {}
             for key, value in job.items():
-                translated_job[mapping[key]] = value
+                translated_job[self.mapping[key]] = value
             translated.append(translated_job)
-
-
-        with open(self.output_dir / 'result.csv', 'w', newline='', encoding='utf-8-sig') as file:
-            writer = csv.DictWriter(file, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerows(translated)
-
+        return translated
 
 
 if __name__ == '__main__':
