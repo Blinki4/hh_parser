e10109bb77602bec24e274d99cd04a30f30a8f03
Blinki4 <dmitriy.velg@mail.ru>
2026-05-05 20:33:49 +0300

    refactoring

--- writers/csv_writer.py
+++ writers/csv_writer.py
@@ -5,7 +5,7 @@ from writers.writer import Writer
 class CsvWriter(Writer):
     def __init__(self):
         super().__init__()
-        self.mapping = {
+        self.headers_mapping = {
             'name' : 'Название',
             'salary' : 'Зарплата',
             'experience' : 'Опыт работы',
@@ -13,6 +13,7 @@ class CsvWriter(Writer):
             'skills' : 'Ключевые навыки',
             'link' : 'Ссылка'
         }
+        self.translated_keys = []
 
 
     def write_parsed_jobs(self, parsed_jobs):
@@ -25,20 +26,22 @@ class CsvWriter(Writer):
 
     def _make_field_names(self):
         field_names = []
-        for key, value in self.mapping.items():
+        for key, value in self.headers_mapping.items():
             field_names.append(value)
         return field_names
 
 
     def translate_keys(self, parsed_jobs):
-        translated = []
         for job in parsed_jobs:
-            translated_job = {}
-            for key, value in job.items():
-                translated_job[self.mapping[key]] = value
-            translated.append(translated_job)
-        return translated
+            self.translate_key(job)
+        return self.translated_keys
 
+
+    def translate_key(self, job):
+        translated_job = {}
+        for key, value in job.items():
+            translated_job[self.headers_mapping[key]] = value
+        self.translated_keys.append(translated_job)
 
 if __name__ == '__main__':
     pass
